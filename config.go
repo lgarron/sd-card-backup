0d5f66b673aa6d95ee2c14f4acc9ade843eb9df3
Lucas Garron <code@garron.net>
2018-04-25 04:32:58 -0700

    Refactor operation method files.

--- config.go
+++ config.go
@@ -2,71 +2,11 @@ package backup
 
 import (
 	"encoding/json"
-	"errors"
-	"fmt"
 	"io/ioutil"
 	"path/filepath"
 
 	"github.com/adrg/xdg"
 )
-
-type folderMapping struct {
-	Source      string `json:"source"`
-	Destination string `json:"destination"`
-}
-
-// Operation defines the config file format for the `sd-card-backup`
-// commandline tool.
-type Operation struct {
-	DestinationRoot  string          `json:"destination_root"`
-	SDCardMountPoint string          `json:"sd_card_mount_point"`
-	SDCardNames      []string        `json:"sd_card_names"`
-	FolderMapping    []folderMapping `json:"folder_mapping"`
-}
-
-func (fm folderMapping) validate() error {
-	if fm.Source == "" {
-		return fmt.Errorf("missing `source` in folder mapping: %+v", fm)
-	}
-	if fm.Destination == "" {
-		return fmt.Errorf("missing `destination` in folder mapping: %+v", fm)
-	}
-	return nil
-}
-
-func (o Operation) validate() error {
-	// TODO: condense calculations similar to table-driven tests.
-	if o.DestinationRoot == "" {
-		return errors.New("missing `destination_root`")
-	}
-	if o.SDCardMountPoint == "" {
-		return errors.New("missing `sd_card_mount_point`")
-	}
-	if o.SDCardNames == nil {
-		return errors.New("missing `sd_card_names`")
-	}
-	if len(o.SDCardNames) == 0 {
-		return errors.New("empty `sd_card_names`")
-	}
-	for _, c := range o.SDCardNames {
-		if c == "" {
-			return errors.New("contains empty SD card name")
-		}
-	}
-	if o.FolderMapping == nil {
-		return errors.New("missing `folder_mapping`")
-	}
-	if len(o.FolderMapping) == 0 {
-		return errors.New("empty `folder_mapping`")
-	}
-	for _, fm := range o.FolderMapping {
-		err := fm.validate()
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
 
 func operationFromBytes(b []byte) (*Operation, error) {
 	config := Operation{}
--- operation.go
+++ operation.go
@@ -1,28 +1,64 @@
 package backup
 
 import (
+	"errors"
 	"fmt"
-	"os"
-	"path/filepath"
 )
 
-func (op Operation) visit(path string, f os.FileInfo, err error) error {
-	if f.IsDir() {
-		return nil
-	}
+type folderMapping struct {
+	Source      string `json:"source"`
+	Destination string `json:"destination"`
+}
 
-	fmt.Printf("[%d] Visited: %s\n", classifyFile(f), path)
+// Operation defines the config file format for the `sd-card-backup`
+// commandline tool.
+type Operation struct {
+	DestinationRoot  string          `json:"destination_root"`
+	SDCardMountPoint string          `json:"sd_card_mount_point"`
+	SDCardNames      []string        `json:"sd_card_names"`
+	FolderMapping    []folderMapping `json:"folder_mapping"`
+}
+
+func (fm folderMapping) validate() error {
+	if fm.Source == "" {
+		return fmt.Errorf("missing `source` in folder mapping: %+v", fm)
+	}
+	if fm.Destination == "" {
+		return fmt.Errorf("missing `destination` in folder mapping: %+v", fm)
+	}
 	return nil
 }
 
-func (op Operation) backupFolder(cardName string, fm folderMapping) {
-	path := filepath.Join(op.SDCardMountPoint, cardName, fm.Source)
-	filepath.Walk(path, op.visit)
-}
-
-// BackupCard backups up the given SD card.
-func (op Operation) BackupCard(cardName string) {
-	for _, fm := range op.FolderMapping {
-		op.backupFolder(cardName, fm)
+func (o Operation) validate() error {
+	// TODO: condense calculations similar to table-driven tests.
+	if o.DestinationRoot == "" {
+		return errors.New("missing `destination_root`")
 	}
+	if o.SDCardMountPoint == "" {
+		return errors.New("missing `sd_card_mount_point`")
+	}
+	if o.SDCardNames == nil {
+		return errors.New("missing `sd_card_names`")
+	}
+	if len(o.SDCardNames) == 0 {
+		return errors.New("empty `sd_card_names`")
+	}
+	for _, c := range o.SDCardNames {
+		if c == "" {
+			return errors.New("contains empty SD card name")
+		}
+	}
+	if o.FolderMapping == nil {
+		return errors.New("missing `folder_mapping`")
+	}
+	if len(o.FolderMapping) == 0 {
+		return errors.New("empty `folder_mapping`")
+	}
+	for _, fm := range o.FolderMapping {
+		err := fm.validate()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
