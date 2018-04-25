151578bdfb97050abbdb0ea183c249f5f8e9e226
Lucas Garron <code@garron.net>
2018-04-25 03:53:51 -0700

    Add config validation (with tests).

--- config.go
+++ config.go
@@ -2,6 +2,8 @@ package backup
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"path/filepath"
 
@@ -22,10 +24,58 @@ type Operation struct {
 	FolderMapping    []folderMapping `json:"folder_mapping"`
 }
 
+func (fm folderMapping) validate() error {
+	if fm.Source == "" {
+		return fmt.Errorf("missing `source` in folder mapping: %+v", fm)
+	}
+	if fm.Destination == "" {
+		return fmt.Errorf("missing `destination` in folder mapping: %+v", fm)
+	}
+	return nil
+}
+
+func (o Operation) validate() error {
+	if o.DestinationRoot == "" {
+		return errors.New("missing `destination_root`")
+	}
+	if o.SDCardMountPoint == "" {
+		return errors.New("missing `sd_card_mount_point`")
+	}
+	if o.SDCardNames == nil {
+		return errors.New("missing `sd_card_names`")
+	}
+	if len(o.SDCardNames) == 0 {
+		return errors.New("empty `sd_card_names`")
+	}
+	for _, c := range o.SDCardNames {
+		if c == "" {
+			return errors.New("contains empty SD card name")
+		}
+	}
+	if o.FolderMapping == nil {
+		return errors.New("missing `folder_mapping`")
+	}
+	if len(o.FolderMapping) == 0 {
+		return errors.New("empty `folder_mapping`")
+	}
+	for _, fm := range o.FolderMapping {
+		err := fm.validate()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func operationFromBytes(b []byte) (*Operation, error) {
 	config := Operation{}
 	// TODO: Enforce required fields to catch config typos?
 	err := json.Unmarshal(b, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	err = config.validate()
 	if err != nil {
 		return nil, err
 	}
--- config_test.go
+++ config_test.go
@@ -3,15 +3,36 @@ package backup
 import (
 	"reflect"
 	"testing"
+
+	"strings"
 )
 
 func TestOperationFromBytesGood(t *testing.T) {
-	s := []byte("{  \"destination_root\": \"/test\",  \"sd_card_mount_point\": \"/Volumes\",  \"sd_card_names\": [    \"HERA\",    \"ZEUS\"  ],  \"folder_mapping\": [    { \"source\": \"DCIM\",                      \"destination\": \"DCIM\"    },    { \"source\": \"MP_ROOT\",                   \"destination\": \"MP_ROOT\" },    { \"source\": \"PRIVATE/AVCHD/BDMV/STREAM\", \"destination\": \"STREAM\"  },    { \"source\": \"PRIVATE/M4ROOT/CLIP\",       \"destination\": \"CLIP\"    }  ]}")
-	expected := &Operation{DestinationRoot: "/test", SDCardMountPoint: "/Volumes", SDCardNames: []string{"HERA", "ZEUS"}, FolderMapping: []folderMapping{folderMapping{Source: "DCIM", Destination: "DCIM"}, folderMapping{Source: "MP_ROOT", Destination: "MP_ROOT"}, folderMapping{Source: "PRIVATE/AVCHD/BDMV/STREAM", Destination: "STREAM"}, folderMapping{Source: "PRIVATE/M4ROOT/CLIP", Destination: "CLIP"}}}
+	s := []byte(`{
+  "destination_root": "/test",
+  "sd_card_mount_point": "/Volumes",
+  "sd_card_names": [
+    "HERA",
+    "ZEUS"
+  ],
+  "folder_mapping": [
+    { "source": "DCIM",                      "destination": "DCIM"    },
+    { "source": "MP_ROOT",                   "destination": "MP_ROOT" }
+  ]
+}`)
+	expected := &Operation{
+		DestinationRoot:  "/test",
+		SDCardMountPoint: "/Volumes",
+		SDCardNames:      []string{"HERA", "ZEUS"},
+		FolderMapping: []folderMapping{
+			folderMapping{Source: "DCIM", Destination: "DCIM"},
+			folderMapping{Source: "MP_ROOT", Destination: "MP_ROOT"},
+		},
+	}
 
 	op, err := operationFromBytes(s)
 	if err != nil {
-		t.Errorf("Unable to read valid config: %s", err)
+		t.Fatalf("Unable to read valid config: %s", err)
 	}
 
 	if !reflect.DeepEqual(expected, op) {
@@ -28,4 +49,82 @@ func TestOperationFromBytesBad(t *testing.T) {
 	}
 }
 
-// TODO: enforce that the config data has all expected entries.
+var validationErrors = []struct {
+	source     string
+	wantPrefix string
+}{
+	{`{
+  "sd_card_mount_point": "/Volumes",
+  "sd_card_names": ["HERA", "ZEUS"],
+  "folder_mapping": [{"source": "from", "destination": "to"}]
+}`,
+		"missing `destination_root`"},
+	{`{
+  "destination_root": "/test",
+  "sd_card_names": ["HERA", "ZEUS"],
+  "folder_mapping": [{"source": "from", "destination": "to"}]
+}`,
+		"missing `sd_card_mount_point`"},
+	{`{
+  "destination_root": "/test",
+  "sd_card_mount_point": "/Volumes",
+  "folder_mapping": [{"source": "from", "destination": "to"}]
+}`,
+		"missing `sd_card_names`"},
+	{`{
+  "destination_root": "/test",
+  "sd_card_mount_point": "/Volumes",
+  "sd_card_names": [],
+  "folder_mapping": [{"source": "from", "destination": "to"}]
+}`,
+		"empty `sd_card_names`"},
+	{`{
+  "destination_root": "/test",
+  "sd_card_mount_point": "/Volumes",
+  "sd_card_names": ["HERA", ""],
+  "folder_mapping": [{"source": "from", "destination": "to"}]
+}`,
+		"contains empty SD card name"},
+	{`{
+  "destination_root": "/test",
+  "sd_card_mount_point": "/Volumes",
+  "sd_card_names": ["HERA", "ZEUS"]
+}`,
+		"missing `folder_mapping`"},
+	{`{
+  "destination_root": "/test",
+  "sd_card_mount_point": "/Volumes",
+  "sd_card_names": ["HERA", "ZEUS"],
+  "folder_mapping": []
+}`,
+		"empty `folder_mapping`"},
+	{`{
+  "destination_root": "/test",
+  "sd_card_mount_point": "/Volumes",
+  "sd_card_names": ["HERA", "ZEUS"],
+  "folder_mapping": [{"destination": "to"}]
+}`,
+		"missing `source` in folder mapping"},
+	{`{
+  "destination_root": "/test",
+  "sd_card_mount_point": "/Volumes",
+  "sd_card_names": ["HERA", "ZEUS"],
+  "folder_mapping": [{"source": "from"}]
+}`,
+		"missing `destination` in folder mapping"},
+}
+
+func TestValidationErrors(t *testing.T) {
+	for _, c := range validationErrors {
+		_, err := operationFromBytes([]byte(c.source))
+
+		if err == nil {
+			t.Errorf("Expected validation error: %#v", c.wantPrefix)
+			continue
+		}
+
+		if !strings.HasPrefix(err.Error(), c.wantPrefix) {
+			t.Errorf("Incorrect prefix.\nPrefix Wanted: %#v\nError Observed: %#v", c.wantPrefix, err.Error())
+		}
+	}
+}
