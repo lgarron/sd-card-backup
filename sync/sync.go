60ae510b694c58557e953747d45290faea363400
Lucas Garron <code@garron.net>
2018-05-23 02:23:16 -0700

    Add rsync-based file copying.
    Shelling out appears to be fairly fast.

--- sync/sync.go
+++ sync/sync.go
@@ -2,6 +2,7 @@ package sync
 
 import (
 	"os"
+	"os/exec"
 	"path/filepath"
 
 	"github.com/mostafah/fsync"
@@ -10,6 +11,8 @@ import (
 // Syncer represents a way to sync a list of files.
 type Syncer interface {
 	Queue(src string, dest string) error
+	// Flushes any queued operations that are not completed, before returning.
+	// Flush() error
 }
 
 // GoSyncer is a Syncer implemented in Go.
@@ -23,10 +26,42 @@ func (s GoSyncer) Queue(src string, dest string) error {
 		return err
 	}
 
-	err = fsync.Sync(dest, src)
-	if err != nil {
-		return err
-	}
+	return fsync.Sync(dest, src)
+}
 
+func (r GoSyncer) Flush() error {
 	return nil
 }
+
+// ImmediateRsync shells out queued files to rsync.
+type ImmediateRsync struct{}
+
+// Queue syncs the given file in an immediate rsync operation (no batching).
+func (r ImmediateRsync) Queue(src string, dest string) error {
+	os.MkdirAll(filepath.Dir(dest), 0700)
+
+	cmd := exec.Command("rsync", "-a", "--", src, dest)
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+// type fileToSync struct {
+//  src  string
+//  dest string
+// }
+
+// // BatchedRsync shells out queued files to rsync, only executing rsync when `Flush` is called.
+// type BatchedRsync struct {
+//  files []fileToSync
+// }
+
+// // Queue queues the given file.
+// func (r BatchedRsync) Queue(src string, dest string) error {
+//  r.files = append(r.files, fileToSync{src: src, dest: dest})
+//  return nil
+// }
+
+// // Flush executesRsync
+// func (r BatchedRsync) Flush() error {
+//  return errors.New("Unimplemented!")
+// }
